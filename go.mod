97352c7b025967ea7a8ba2b2f9997c03403cb24c
Mihalis Tsoukalos <mihalistsoukalos@gmail.com>
2018-10-25 17:42:18 +0300

    Initial version

---
+++ go.mod
@@ -0,0 +1,1 @@
+module github.com/mactsouk/go_version
---
+++ go_version.go
@@ -0,0 +1,13 @@
+package go_version
+
+import (
+	"fmt"
+)
+
+func init() {
+	fmt.Println("init()")
+}
+
+func FromG_V() {
+	fmt.Println("FromG_V()")
+}
